638d7341e61f9bedbacd76b89e0f223452697c24
emilycain92 <cain.emily@outlook.com>
2021-11-06 14:18:47 -0400

    beginning css

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Start of Variables//
+var header = document.querySelector(".header");
+var highestScore = document.querySelector(".highScores");
+var displayTime = document.querySelector(".displayTime");
