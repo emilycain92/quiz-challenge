4c2c711f3cdd760211af5982cbb363cc9e95d083
emilycain92 <you@example.com>
2021-12-14 15:31:22 -0500

    finished html, started css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-//Start of Variables//
-var header = document.querySelector(".header");
-var highestScore = document.querySelector(".highScores");
-var displayTime = document.querySelector(".displayTime");
+var containerQuestionEl = document.getElementById("question-container");
+var containerStartEl = document.getElementById("starter-container");
+var containerEndEl = document.getElementById("end-container")
+var containerScoreEl = document.getElementById("score-banner")
+var formInitials = document.getElementById("initials-form")
+var containerHighScoresEl = document.getElementById("high-score-container")
+var ViewHighScoreEl = document.getElementById("view-high-scores")
+var listHighScoreEl = document.getElementById("high-score-list")
+var correctEl = document.getElementById("correct")
+var wrongEl = document.getElementById("wrong")
+//buttons 
+var btnStartEl = document.querySelector("#start-game");
+var btnGoBackEl = document.querySelector("#go-back")
+var btnClearScoresEl = document.querySelector("#clear-high-scores")
+//questions/answers
+var questionEl = document.getElementById("question")
+var answerbuttonsEl = document.getElementById("answer-buttons")
+var timerEl = document.querySelector("#timer");
+var score = 0;
+var timeleft;
+var gameover
+timerEl.innerText = 0;
